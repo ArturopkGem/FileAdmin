1c6cf0b89e22ecae6389230db26db25d5837755c
Sergio Noh <serganoh@uacam.mx>
2026-09-14 09:35:57 -0600

    Act_Stream, updated previus to homework

--- Practica_01/Act_Stream.java
+++ Practica_01/Act_Stream.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class Act_Stream {
     public static void escribir( ) throws IOException {
@@ -20,45 +19,11 @@ public class Act_Stream {
         fis.close();
     }
 
-    public static void leerTryWith(){
-        try (InputStream fis = new FileInputStream("ListaAsistencia.txt")){
-            int byteFile;
-            byteFile = fis.read();
-            System.out.println(byteFile);
-        } catch (IOException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void escribirObjectoTryWith(){
-        try (OutputStream fos = new FileOutputStream("object_data.txt");
-             ObjectOutputStream oos = new ObjectOutputStream(fos);){
-            ArrayList<String> listaAsistencia = new ArrayList<>();
-            listaAsistencia.add("Arturo");
-            listaAsistencia.add("Cecilia");
-            listaAsistencia.add("Diego");
-            listaAsistencia.add("Fernando");
-
-            oos.writeBoolean(true);
-            oos.writeUTF("Hola Mundo!");
-            oos.writeObject(listaAsistencia);
-        } catch (IOException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void leerObjectoTryWith(){
-        try (InputStream fis = new FileInputStream("object_data.txt");
-             ObjectInputStream ois = new ObjectInputStream(fis);){
-
-            System.out.println(ois.readBoolean());
-            System.out.println(ois.readUTF());
-            ArrayList<String> ListaAsistenciaInput = (ArrayList<String>) ois.readObject();
-            System.out.println("Total de Estudiantes: "+ListaAsistenciaInput.size());
-        } catch (IOException e){
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public static void guardarNota (String nota) {
+//        System.out.println("guardar Nota");
+//    }
+//
+//    public static String leerNotas() {
+//        return "notas";
+//    }
 }
